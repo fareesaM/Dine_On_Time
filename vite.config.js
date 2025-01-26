b46b9b86cbef14a05905b8f6775c77a9902a5247
fareesaM <fareesamas@gmail.com>
2025-01-26 15:59:31 +0300

    render deployed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Dine_On_Time',
-})
+  base: "/Dine_On_Time",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fsd-dot-bknd.onrender.com', // Your backend URL
+        changeOrigin: true,
+        secure: true, // Ensure this is `true` for HTTPS
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
